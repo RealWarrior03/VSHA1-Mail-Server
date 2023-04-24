c0af5a0993bbbc0de003e88bc19fba5955402b50
lukasbloemer <lukas.bloemer.ol@gmail.com>
2023-04-24 14:20:59 +0200

    Init

---
+++ VerteilteProbleme1/src/main/java/org/example/Main.java
@@ -0,0 +1,52 @@
+package org.example;
+import java.io.IOException;
+import java.net.*;
+import java.nio.*;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        serverSocketChannel.bind(new InetSocketAddress("localhost",25));
+
+
+        serverSocketChannel.configureBlocking(false);
+
+
+        Selector selector = Selector.open();
+        serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+        // listen for incoming client connections
+        System.out.println("Server listening on port 2525");
+        while (true) {
+            selector.select();
+            for (SelectionKey key : selector.selectedKeys()) {
+                if (key.isAcceptable()) {
+                    // handle the incoming client connection
+                    System.out.println("Acceptable Key received!");
+                    SocketChannel clientSocketChannel = serverSocketChannel.accept();
+                    clientSocketChannel.configureBlocking(false);
+                    clientSocketChannel.register(selector, SelectionKey.OP_READ);
+                    System.out.println("Client connected!");
+                } else if (key.isReadable()) {
+                    // handle the incoming data from the client
+                    SocketChannel clientSocketChannel = (SocketChannel) key.channel();
+                    ByteBuffer buffer = ByteBuffer.allocate(1024);
+                    clientSocketChannel.read(buffer);
+                    buffer.flip();
+                    byte[] bytes = new byte[buffer.remaining()];
+                    buffer.get(bytes);
+                    String message = new String(bytes);
+                    System.out.println("Received message: " + message);
+                    clientSocketChannel.close();//!!Falsch!!
+                    System.out.println("Client disconnected!");
+                }
+            }
+            selector.selectedKeys().clear();
+        }
+    }
+}
