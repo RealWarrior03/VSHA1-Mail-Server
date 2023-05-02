634fe39eeb5b96dcacb75d4c0ba86f06d93640a7
lukasbloemer <lukas.bloemer.ol@gmail.com>
2023-05-02 14:22:36 +0200

    Klammer hat gefehlt

--- Main.java
+++ Main.java
@@ -53,7 +53,7 @@ public class Main {
         Selector selector = Selector.open();
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
-        HashMap<SocketChannel,MailInfo> activeMailInfos = new HashMap<>();
+        HashMap<SocketChannel, MailInfo> activeMailInfos = new HashMap<>();
 
         // listen for incoming client connections
         System.out.println("Server listening on port 2525");
@@ -94,13 +94,13 @@ public class Main {
 
                     String payload;
                     //checks which command is sent by the client
-                    if(activeMailInfos.get(clientSocketChannel).getIsWriting()){
-                        if(message.substring(message.length()-6)=="\r\n.\r\n"){
+                    if (activeMailInfos.get(clientSocketChannel).getIsWriting()) {
+                        if (message.substring(message.length() - 6) == "\r\n.\r\n") {
                             activeMailInfos.get(clientSocketChannel).setIsWriting(false);
-                            message = message.substring(0,message.length()-6);
+                            message = message.substring(0, message.length() - 6);
                         }
                         activeMailInfos.get(clientSocketChannel).appendData(message);
-                        if(!activeMailInfos.get(clientSocketChannel).isWriting){
+                        if (!activeMailInfos.get(clientSocketChannel).isWriting) {
                             activeMailInfos.get(clientSocketChannel).storeMail();
                             activeMailInfos.remove(clientSocketChannel);
                         }
@@ -175,43 +175,43 @@ public class Main {
                                     response = "500 Command unrecognized, send \"HELP\"  for more information.\r\n";
                                 }
                         }
-                    switch (message.substring(0, Math.min(message.length(), 4))) { //check commands with len 4 (math.min prevents an out of bounds error
-                        case "HELO":
-                            payload = message.substring(4, message.length()-2);
-                            response = "250 " + hostname + " \r\n";
-                            break;
-                        case "DATA":
-                            payload = message.substring(4, message.length()-2);
-                            System.out.println("Handling Data Packet");
-                            byte [] test = {'T','e','s','t'};
-                            ByteBuffer buf = ByteBuffer.allocate(8);
-                            buf.put(test);
-                            buf.flip();
-                            FileOutputStream f;
-                            f = new FileOutputStream("test.txt");
-                            FileChannel ch = f.getChannel();
-                            ch.write(buf);
-                            ch.close();
-                            buf.clear();
-                            break;
-                        case "HELP":
-                            payload = message.substring(4, message.length()-2);
-                            String code = "214";
+                        switch (message.substring(0, Math.min(message.length(), 4))) { //check commands with len 4 (math.min prevents an out of bounds error
+                            case "HELO":
+                                payload = message.substring(4, message.length() - 2);
+                                response = "250 " + hostname + " \r\n";
+                                break;
+                            case "DATA":
+                                payload = message.substring(4, message.length() - 2);
+                                System.out.println("Handling Data Packet");
+                                byte[] test = {'T', 'e', 's', 't'};
+                                ByteBuffer buf = ByteBuffer.allocate(8);
+                                buf.put(test);
+                                buf.flip();
+                                FileOutputStream f;
+                                f = new FileOutputStream("test.txt");
+                                FileChannel ch = f.getChannel();
+                                ch.write(buf);
+                                ch.close();
+                                buf.clear();
+                                break;
+                            case "HELP":
+                                payload = message.substring(4, message.length() - 2);
+                                String code = "214";
 
-                            response = code + """
-                                     The following commands are supported:
-                                    HELO - The HELO command initiates the SMTP session conversation. The client greets the server and introduces itself. As a rule, HELO is attributed with an argument that specifies the domain name or IP address of the SMTP client.
-                                    MAIL FROM - The MAIL FROM command initiates a mail transfer. As an argument, MAIL FROM includes a sender mailbox (reverse-path).
-                                    RCPT TO - The RCPT TO command specifies the recipient. As an argument, RCPT TO includes a destination mailbox (forward-path). In case of multiple recipients, RCPT TO will be used to specify each recipient separately.
-                                    DATA - With the DATA command, the client asks the server for permission to transfer the mail data. The response code 354 grants permission, and the client launches the delivery of the email contents line by line. This includes the date, from header, subject line, to header, attachments, and body text.
-                                    HELP [command] - With the HELP command, the client requests a list of commands the server supports. HELP may be used with an argument (a specific command).
-                                    QUIT - The QUIT command send the request to terminate the SMTP session. Once the server responses with 221, the client closes the SMTP connection.
-                                    
-                                    The explanantions of the commands above are taken from the following website: https://mailtrap.io/blog/smtp-commands-and-responses/#HELP
-                                    \r\n
-                                    """;
+                                response = code + """
+                                         The following commands are supported:
+                                        HELO - The HELO command initiates the SMTP session conversation. The client greets the server and introduces itself. As a rule, HELO is attributed with an argument that specifies the domain name or IP address of the SMTP client.
+                                        MAIL FROM - The MAIL FROM command initiates a mail transfer. As an argument, MAIL FROM includes a sender mailbox (reverse-path).
+                                        RCPT TO - The RCPT TO command specifies the recipient. As an argument, RCPT TO includes a destination mailbox (forward-path). In case of multiple recipients, RCPT TO will be used to specify each recipient separately.
+                                        DATA - With the DATA command, the client asks the server for permission to transfer the mail data. The response code 354 grants permission, and the client launches the delivery of the email contents line by line. This includes the date, from header, subject line, to header, attachments, and body text.
+                                        HELP [command] - With the HELP command, the client requests a list of commands the server supports. HELP may be used with an argument (a specific command).
+                                        QUIT - The QUIT command send the request to terminate the SMTP session. Once the server responses with 221, the client closes the SMTP connection.
+                                                                            
+                                        The explanantions of the commands above are taken from the following website: https://mailtrap.io/blog/smtp-commands-and-responses/#HELP
+                                        \r\n
+                                        """;
 
-                            //TODO different help cases?
+                                //TODO different help cases?
                             /*
                             if(message.substring(0, Math.min(message.length(), 9)).equals("HELP HELO")) { //check for rcpt to command
                                 response = "help for HELO coming soon\r\n";
@@ -253,37 +253,38 @@ public class Main {
                             System.out.println(response);
                              */
 
-                            break;
-                        case "QUIT":
-                            payload = message.substring(4, message.length()-2);
-                            response =  "221 " + hostname;
-                            //TODO maybe kick client from selectors
-                            break;
-                        default: //command doesn't match any len 4 command
-                            if(message.substring(0, Math.min(message.length(), 9)).equals("RCPT TO: ")) { //check for rcpt to command
-                                payload = message.substring(9, message.length()-2);
-                                String rcpt = message.substring(9,message.length()-4);
-                                activeMailInfos.get(clientSocketChannel).addRCPT(rcpt);
-                                response = "250 OK\r\n";
-                            } else if(message.substring(0, Math.min(message.length(), 11)).equals("MAIL FROM: ")) { //check for mail from command
-                                payload = message.substring(11, message.length()-2);
-                                activeMailInfos.put(clientSocketChannel,new MailInfo(clientSocketChannel));
-                                String sender = message.substring(11,message.length()-4); // TODO: Ersetzen durch eigentliche Message
-                                activeMailInfos.get(clientSocketChannel).setSender(sender);
-                                response = "250 OK\r\n";
-                            } else {
-                                response = "500 Command unrecognized, send \"HELP\"  for more information.\r\n";
-                            }
-                    }
-                    clientSocketChannel.write(ByteBuffer.wrap(response.getBytes()));
+                                break;
+                            case "QUIT":
+                                payload = message.substring(4, message.length() - 2);
+                                response = "221 " + hostname;
+                                //TODO maybe kick client from selectors
+                                break;
+                            default: //command doesn't match any len 4 command
+                                if (message.substring(0, Math.min(message.length(), 9)).equals("RCPT TO: ")) { //check for rcpt to command
+                                    payload = message.substring(9, message.length() - 2);
+                                    String rcpt = message.substring(9, message.length() - 4);
+                                    activeMailInfos.get(clientSocketChannel).addRCPT(rcpt);
+                                    response = "250 OK\r\n";
+                                } else if (message.substring(0, Math.min(message.length(), 11)).equals("MAIL FROM: ")) { //check for mail from command
+                                    payload = message.substring(11, message.length() - 2);
+                                    activeMailInfos.put(clientSocketChannel, new MailInfo(clientSocketChannel));
+                                    String sender = message.substring(11, message.length() - 4); // TODO: Ersetzen durch eigentliche Message
+                                    activeMailInfos.get(clientSocketChannel).setSender(sender);
+                                    response = "250 OK\r\n";
+                                } else {
+                                    response = "500 Command unrecognized, send \"HELP\"  for more information.\r\n";
+                                }
+                        }
+                        clientSocketChannel.write(ByteBuffer.wrap(response.getBytes()));
 
                     /*
                     clientSocketChannel.close();//!!Falsch!!
                     System.out.println("Client disconnected!");
                     */
+                    }
                 }
+                selector.selectedKeys().clear();
             }
-            selector.selectedKeys().clear();
         }
     }
 }
